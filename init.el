e2ee1d1cfbfab54fe26d1ed4ff14aa99c1348585
Murphy McMahon <pandeiro@gmail.com>
2011-12-25 12:28:49 -0200

    added automatic package installation to init.el

--- init.el
+++ init.el
@@ -5,6 +5,16 @@
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
+;; More stealing from technomancy
+(setq marmalade-server "http://marmalade-repo.org/")
+(when (null package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(clojure-mode clojurescript-mode magit paredit))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+                      
 (require 'my-general)
 (require 'my-keys)
 (require 'my-clojure)
