76ceb1d6745a966c93d219b4bed759cc87a8a4ad
Murph <pandeiro@gmail.com>
2012-11-22 18:34:47 -0200

    added org-mode's suggested global C-c bindings

--- my-keys.el
+++ my-keys.el
@@ -10,6 +10,13 @@
 (global-set-key (kbd "C-c r") 'make-lein-run-buffer)
 (global-set-key (kbd "C-c b") 'make-lein-cljsbuild-buffer)
 
+;; As suggested by org-mode itself
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+
 ; http://inversethought.com/hg/hgwebdir.cgi/dotemacs/file/5f95b63347ec/dotemacs.el
 ; NOTE: These shortcuts work in xterm but the up/down ones do NOT work in gnome-terminal
 (global-set-key (kbd "C-S-<down>") 'windmove-down)
