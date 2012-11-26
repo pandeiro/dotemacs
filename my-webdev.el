cce3ad55b0ce991dd963a54b7402ffc51e63c8bf
Murph <pandeiro@gmail.com>
2012-11-26 11:30:03 -0200

    disable elnode default webserver launching automatically

--- my-webdev.el
+++ my-webdev.el
@@ -5,4 +5,12 @@
 (defun in-rainbows () (rainbow-mode 1))
 (add-hook 'css-mode-hook 'in-rainbows)
 
+;;
+;; Elnode customizations
+;;
+;; Don't start webserver automatically
+(setq elnode-do-init nil)
+
+;; todo: customize elnode-webserver-index-file-template & elnode-webserver-index-page-template
+
 (provide 'my-webdev)
