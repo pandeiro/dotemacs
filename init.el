9e479d2fe5663c9a5b5fb4680169588d5adeadbe
Murph <pandeiro@gmail.com>
2012-11-29 09:46:04 -0200

    require my-ui package

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
     (package-install p)))
 
 (require 'my-general)
+(require 'my-ui)
 (require 'my-webdev)
 (require 'my-util)
 (require 'my-keys)
@@ -36,17 +37,4 @@
 (require 'my-org)
 (require 'my-go)
 (require 'my-erc)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector [solarized-bg red green yellow blue magenta cyan solarized-fg])
- '(custom-safe-themes (quote ("d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" default)))
- '(fci-rule-color "#073642"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
