07e18535dc29bf02792e70ed11dfa7ddc0908c9a
Murph <pandeiro@gmail.com>
2012-11-29 09:45:38 -0200

    separate out ui-specific code

--- my-general.el
+++ my-general.el
@@ -3,23 +3,6 @@
 (setq-default save-place t)
 ;(desktop-save-mode 1)
 
-;; Line numbers (deactivated, wastes space)
-;(global-linum-mode 1)
-
-;; Tchau, Menubar, Toolbar, big Fringe
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-(set-fringe-mode '(1 . 1))
-
-;; Custom scrollbars now defined in ~/.config/gtk-3.0/gtk.css
-; (scroll-bar-mode -1)
-
-;; Parenthesis matching
-(show-paren-mode t)
-
-;; Default font
-(set-face-attribute 'default nil :height 90 :family "Inconsolata")
-
 ;; Paredit in Clojure and Lisp modes
 (defun turn-on-paredit () (paredit-mode 1))
 (add-hook 'clojure-mode-hook 'turn-on-paredit)
@@ -58,9 +41,6 @@
   (xterm-register-default-colors)
   (tty-set-up-initial-frame-faces))
 
-;; Turn on ansi-color-for-comint-mode for shell output buffers
-(ansi-color-for-comint-mode-on)
-
 ;; Auto-update buffers after a git branch change
 ;; http://stackoverflow.com/questions/1480572/how-to-have-emacs-auto-refresh-all-buffers-when-files-have-changed-on-disk
 (global-auto-revert-mode t)
@@ -74,15 +54,5 @@
 
 ;; Setup ESS mode - commented while not doing stats work
 ;(require 'ess-site)
-
-;; Load Workgroups and use w/ prefix C-c w
-(require 'workgroups)
-(setq wg-prefix-key (kbd "C-c w"))
-(setq wg-switch-on-load nil)
-(setq wg-morph-on nil)
-;(setq wg-morph-hsteps 8)
-;(setq wg-morph-vsteps 8)
-(workgroups-mode 1)
-(wg-load "~/.workgroups-emacs")
 
 (provide 'my-general)
---
+++ my-ui.el
@@ -0,0 +1,38 @@
+;; Line numbers (deactivated, wastes space)
+;(global-linum-mode 1)
+
+;; Tchau, Menubar, Toolbar, big Fringe
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(set-fringe-mode '(1 . 1))
+
+;; Custom scrollbars now defined in ~/.config/gtk-3.0/gtk.css
+; (scroll-bar-mode -1)
+
+;; Parenthesis matching
+(show-paren-mode t)
+
+;; Default font
+(set-face-attribute 'default nil :height 90 :family "Inconsolata")
+
+;; Use Solarized with one custom tweak: no fringes
+(load-theme 'solarized-light 1)
+(custom-theme-set-faces
+ 'solarized-light
+ `(fringe ((((class color) (min-colors 89))
+	    (:foreground nil :background nil)))))
+
+;; Turn on ansi-color-for-comint-mode for shell output buffers
+(ansi-color-for-comint-mode-on)
+
+;; Load Workgroups and use w/ prefix C-c w
+(require 'workgroups)
+(setq wg-prefix-key (kbd "C-c w"))
+(setq wg-switch-on-load nil)
+(setq wg-morph-on nil)
+;(setq wg-morph-hsteps 8)
+;(setq wg-morph-vsteps 8)
+(workgroups-mode 1)
+(wg-load "~/.workgroups-emacs")
+
+(provide 'my-ui)
