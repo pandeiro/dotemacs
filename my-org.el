11893117e77e7922a71d6a5d22965c714f841053
Murph <pandeiro@gmail.com>
2012-12-27 19:01:54 -0200

    use nrepl with org-babel instead of swank/slime

--- my-org.el
+++ my-org.el
@@ -80,5 +80,30 @@
    (python . t)
    (sh . t)))
 
+;; Clojure-specific
+;;
+;; org-babel Clojure support is built to work with Swank/SLIME, which is no longer
+;; being developed, having been replaced by nREPL. This code switches out the former
+;; in favor of the latter. Taken from https://github.com/lambdatronic/org-babel-example
+
+;; Patch ob-clojure to work with nrepl
+(declare-function nrepl-send-string-sync "ext:nrepl" (code &optional ns))
+
+(defun org-babel-execute:clojure (body params)
+  "Execute a block of Clojure code with Babel."
+  (require 'nrepl)
+  (with-temp-buffer
+    (insert (org-babel-expand-body:clojure body params))
+    ((lambda (result)
+       (let ((result-params (cdr (assoc :result-params params))))
+         (if (or (member "scalar" result-params)
+                 (member "verbatim" result-params))
+             result
+           (condition-case nil (org-babel-script-escape result)
+             (error result)))))
+     (plist-get (nrepl-send-string-sync
+                 (buffer-substring-no-properties (point-min) (point-max))
+                 (cdr (assoc :package params)))
+                :value))))
 
 (provide 'my-org)
