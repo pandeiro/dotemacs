e4a5b0334192e50cd416c64aa6ca9fe679821ff1
Murph <pandeiro@gmail.com>
2012-12-27 19:08:20 -0200

    fix nodejs prompt in js-comint

--- my-webdev.el
+++ my-webdev.el
@@ -16,4 +16,19 @@
 
 ;; todo: customize elnode-webserver-index-file-template & elnode-webserver-index-page-template
 
+;; Use nodejs as inferior javascript process
+(require 'js-comint)
+(setq inferior-js-program-command "node")
+
+;; Fix nodejs prompt
+(setq inferior-js-mode-hook
+      (lambda ()
+        ;; We like nice colors
+        (ansi-color-for-comint-mode-on)
+        ;; Deal with some prompt nonsense
+        (add-to-list
+         'comint-preoutput-filter-functions
+         (lambda (output)
+           (replace-regexp-in-string "\033\\[[0-9]+[GJK]" "" output)))))
+
 (provide 'my-webdev)
