ed73caf4a2b8e1e91509d55fc5371bf85ea9490f
Murph <pandeiro@gmail.com>
2012-12-29 12:55:23 -0200

    adjust completing-read to be able to use ido

--- my-general.el
+++ my-general.el
@@ -21,6 +21,34 @@
 (require 'ido)
 (ido-mode t)
 
+;; Use ido for everything if this is turned on
+;; http://emacswiki.org/emacs/InteractivelyDoThings#toc14
+(defvar ido-enable-replace-completing-read nil
+  "If t, use ido-completing-read instead of completing-read if possible.
+
+  Set it to nil using let in around-advice for functions where the
+  original completing-read is required.  For example, if a function
+  foo absolutely must use the original completing-read, define some
+  advice like this:
+
+  (defadvice foo (around original-completing-read-only activate)
+    (let (ido-enable-replace-completing-read) ad-do-it))")
+
+;; Replace completing-read wherever possible, unless directed otherwise
+(defadvice completing-read
+  (around use-ido-when-possible activate)
+  (if (or (not ido-enable-replace-completing-read) ; Manual override disable ido
+          (and (boundp 'ido-cur-list)
+               ido-cur-list)) ; Avoid infinite loop from ido calling this
+      ad-do-it
+    (let ((allcomp (all-completions "" collection predicate)))
+      (if allcomp
+          (setq ad-return-value
+                (ido-completing-read prompt
+                                     allcomp
+                                     nil require-match initial-input hist def))
+        ad-do-it))))
+
 ;; Emacs adjustments made here, bound to C-c e
 (defun open-my-config-dir ()
   (interactive)
