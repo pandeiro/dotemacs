e5fcb259fedf663fe00f62f5ddfc3d202433fbe1
Murph <pandeiro@gmail.com>
2013-01-10 14:58:01 -0200

    simplify startup msg in *scratch*

--- my-scratch.el
+++ my-scratch.el
@@ -1,18 +1,9 @@
 ;; Open straight to *scratch*, no message, org-mode
 (setq initial-major-mode 'org-mode)
 (setq initial-scratch-message
-"You are in *scratch* w/ org-mode:
-
-C-x C-e   eval lisp              anywhere
-C-c C-c   eval anything          in src blocks
-C-c C-h   highlight code syntax  here
-C-c '     edit src               in other buffer
-
-C-c n     new project            prompt
-
-Have fun!
-
-* ")
+"* First
+   
+")
 
 ;; No startup screen
 (setq inhibit-startup-screen 1)
