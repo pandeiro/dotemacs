c05060aa8a84fa8456603193a99dc3e1ae701e7e
Murph <pandeiro@gmail.com>
2013-02-01 14:01:36 -0200

    added melpa

--- init.el
+++ init.el
@@ -3,6 +3,8 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" .  "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
 ;; More stealing from technomancy
