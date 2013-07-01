ec89e500a4163e36ab541a6f5920117cdc1e56fa
Murph <pandeiro@gmail.com>
2013-07-01 20:15:30 -0300

    set js2-mode basic offset (2)

--- my-webdev.el
+++ my-webdev.el
@@ -1,5 +1,6 @@
 ;; JavaScript 2-space indent
 (setq js-indent-level 2)
+(setq js2-basic-offset 2) ; js2-mode equivalent
 
 ;; Turn on rainbow-mode automatically with css-mode
 (defun in-rainbows () (rainbow-mode 1))
@@ -17,12 +18,16 @@
 ;; Fix nodejs prompt
 (setq inferior-js-mode-hook
       (lambda ()
-        ;; We like nice colors
-        (ansi-color-for-comint-mode-on)
-        ;; Deal with some prompt nonsense
-        (add-to-list
-         'comint-preoutput-filter-functions
-         (lambda (output)
-           (replace-regexp-in-string "\033\\[[0-9]+[GJK]" "" output)))))
+	;; We like nice colors
+	(ansi-color-for-comint-mode-on)
+	;; Deal with some prompt nonsense
+	(add-to-list
+	 'comint-preoutput-filter-functions
+	 (lambda (output)
+	   (replace-regexp-in-string "\033\\[[0-9]+[GJK]" "" output)))))
+
+;; Use web-mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 
 (provide 'my-webdev)
