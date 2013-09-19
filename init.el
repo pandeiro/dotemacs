61943d1934d366c82cfca8e998b559f9d659f8bc
Murph <pandeiro@gmail.com>
2013-09-19 10:57:07 -0300

    do load-path correctly

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 (load custom-file)
 
 ;; Package management with Marmalade repo
-(add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/"))
 (require 'package)
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
