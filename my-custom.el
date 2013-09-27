25cff236333a9bf7f14f13eebc9749ff4a5b73ad
Murph <pandeiro@gmail.com>
2013-09-27 13:50:55 -0300

    customizations to eliminate some org-export cruft

---
+++ my-custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-export-author-info nil)
+ '(org-export-with-section-numbers nil)
+ '(org-export-with-toc nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
