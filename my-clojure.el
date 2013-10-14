5e6c50a2d2366d77159c0289b4b7585cc2d7bb39
Murph <pandeiro@gmail.com>
2013-10-14 13:41:26 -0300

    pretty lambdas (stolen from emacs-live)

--- my-clojure.el
+++ my-clojure.el
@@ -139,4 +139,26 @@ if use-rhino-repl has been invoked")
     (nrepl-jack-in)
     (cd current-dir)))
 
+;; Taken from Emacs Live
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "λ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\)("
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "ƒ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\){"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "∈")
+                               nil))))))
+
 (provide 'my-clojure)
