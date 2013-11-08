f863d441b9e3c02b4e530b3695e130e52c30697e
Murph <pandeiro@gmail.com>
2013-11-07 23:33:34 -0200

    update autojoins

--- my-erc.el
+++ my-erc.el
@@ -2,7 +2,8 @@
 (erc-autojoin-mode 1)
 (setq erc-autojoin-channels-alist
       '(("freenode.net" "#emacs" "#org-mode" "#clojure" "##javascript" "#archlinux"
-	 "##linux" "#bash" "#git" "#html5" "##twitter-bootstrap" "#couchdb" "#go-nuts")
+	 "##linux" "#bash" "#git" "#html5" "##twitter-bootstrap" "#couchdb" "#go-nuts"
+	 "#angularjs")
 	("oftc.net" "#awesome")))
 
 ;; http://emacs-fu.blogspot.com/2009/06/erc-emacs-irc-client.html
