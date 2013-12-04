934c5e5698087216643092679d54c420502f1655
Murph <pandeiro@gmail.com>
2013-12-04 11:15:07 -0200

    specify pandoc bin

--- my-custom.el
+++ my-custom.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(org-export-author-info nil)
  '(org-export-with-section-numbers nil)
- '(org-export-with-toc nil))
+ '(org-export-with-toc nil)
+ '(pandoc-binary "~/.cabal/bin/pandoc"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
