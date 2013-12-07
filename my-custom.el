49e7c70c3746909f4aba2020654e60de33ff2838
Murph <pandeiro@gmail.com>
2013-12-07 14:33:27 -0200

    w3m link face colors

--- my-custom.el
+++ my-custom.el
@@ -12,4 +12,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(w3m-anchor ((t (:inherit compilation-info-face))))
+ '(w3m-arrived-anchor ((t (:inherit escape-glyph))))
+ '(w3m-arrived-anchor-face ((t (:inherit escape-glyph))) t))
